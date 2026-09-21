dd566ba48bfe99949e7a6c8beeff1f9cf8bf3954
Виталий <vitalie01rus01@gmail.com>
2026-09-21 18:48:26 +0300

    Для refresh токена добавлены модель и репозиторий

---
+++ src/main/java/ru/vitalii/task_manager/model/RefreshToken.java
@@ -0,0 +1,35 @@
+package ru.vitalii.task_manager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, columnDefinition = "TEXT")
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    private String replacedByToken;
+}
